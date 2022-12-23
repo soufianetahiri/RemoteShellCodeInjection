2f32f0df269d3196ea7030041801fc4fc18549a9
Soufiane Tahiri <soufianetahiri@gmail.com>
2022-12-23 20:23:43 +0100

    added SharpUnhooker

---
+++ RemoteShellCodeInjection/Utils/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteShellCodeInjection.Utils
+{
+    public static class Utils
+    {
+        public static string FromHexString(string hexString)
+        {
+            var bytes = new byte[hexString.Length / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+
+        //totaly stolen from : https://github.com/san3ncrypt3d/AESShellCodeInjector/blob/main/AESInject/AESInject/Program.cs
+        public static byte[] AESDecrypt(byte[] CEncryptedShell, byte[] key, byte[] iv)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.KeySize = 128;
+                aes.BlockSize = 128;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    return GetDecrypt(CEncryptedShell, decryptor);
+                }
+            }
+        }
+        private static byte[] GetDecrypt(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            using (var ms = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
